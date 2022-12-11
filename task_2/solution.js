752dfd11bf9488310adb6167c204ac985e99b895
Olga Obozova <Olga-lim-1985@mail.ru>
2022-12-11 18:24:16 +0300

    task 2 DONE

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,7 +9,7 @@ if (productsSum == 0) {
     shippingSum = 0;
 } else if (productsSum >= freeShippingMinSum) {
     shippingSum = 0;
-} else ((productsSum > 0) && (productsSum > freeShippingMinSum)) {
+} else {
     shippingSum = shippingPrice;
 } 
 
